b82865654faa7fcfb2a86251e8c42b748e1cbba5
rafaqz <rafaelschouten@gmail.com>
2022-08-17 08:34:01 +0000

    put ports in their own file

--- raster_generation.jl
+++ raster_generation.jl
@@ -1,4 +1,5 @@
 includet("raster_common.jl")
+includet("ports.jl")
 
 # Distances
 
@@ -13,58 +14,6 @@ foreach(island_keys, distance_to_coasts) do i, raster
     write(joinpath(distancedir, string(i), "to_coast.tif"), raster)
 end
 
-# To ports
-# In both cases we have a shift in the
-# location of the major port.
-ports = (   
-    mus=(;
-        major1=(;
-            Grand_Port=(-20.3754, 57.7228),
-        ),
-        major2=(;
-            Port_Louie=(-20.1597, 57.5012),
-        ),
-        minor=(;
-            Albion=(-20.218, 57.402),
-            Baie_du_Cap=(-20.491, 57.377),
-            Bel_Ombre=(-20.503, 57.399),
-            Black_River=(-20.362, 57.372),
-            Cap=(-19.986, 57.621),
-            Flic_en_Flac=(-20.275, 57.371),
-            Grand_Baie=(-20.013, 57.584),
-            Grand_Port=(-20.3754, 57.7228),
-            Grand_River_South_East=(-20.288, 57.784),
-            Grande_Gaube=(-20.009, 57.670),
-            La_Gaulette=(-20.427, 57.360),
-            Mahebourg=(-20.405, 57.709),
-            Port_Louie=(-20.1597, 57.5012),
-            Poste_de_Flacq=(-20.163, 57.743),
-            Poudre_de_Or=(-20.0610, 57.685),
-            Souillac=(-20.518, 57.517),
-            Terre_Rouge=(-20.139, 57.499),
-            Trou_dEau_Douce=(-20.244, 57.787),
-            Turtle_Bay=(-20.0834, 57.516),
-        ),
-    ),
-    reu=(;
-        major1=(;
-            Saint_Denis=(-20.876, 55.446),
-        ),
-        major2=(;
-            Le_Port=(-20.937, 55.293),
-        ),
-        minor=(;
-            Le_Port=(-20.937, 55.293),
-            Saint_Paul=(-21.008, 55.270),
-            Saint_Benoit=(-21.0392, 55.722),
-            Saint_Denis=(-20.876, 55.446),
-            Saint_Joseph=(-21.389, 55.644),
-            Saint_Pierre=(-21.344, 55.481),
-            Saint_Phillipe=(-21.364, 55.767),
-            Sale=(-21.269, 55.335),
-        ),
-    ),
-)
 
 foreach(island_keys, dems, ports) do i, dem, classes
     foreach(namedkeys(classes), classes) do k, locations
