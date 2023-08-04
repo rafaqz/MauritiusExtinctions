9fcd08c566616e7e799ef917ea3a7981490482f6
Rafael Schouten <rafaelschouten@gmail.com>
2023-08-04 22:47:01 +0200

    more plots

--- iucn.jl
+++ iucn.jl
@@ -5,6 +5,7 @@ using GLMakie
 using TextAnalysis
 using Plots
 using StatsPlots
+using DimensionalData.LookupArrays
 
 # IUCNRedList.set_token("486f559a61285ba396234fc186897b94eda1bd15aa4216a8e1d9f4a8cf40d4c7")
 # spec = species_by_category("EX") 
@@ -40,7 +41,8 @@ pairs(names(iucn_assesment))
 iucn_assesment.uncertainty_bool = map(iucn_assesment.uncertainty) do u
     ismissing(u) || u == "Yes"
 end
-iucn_assesment.decade_last_seen = iucn_assesment.yearLastSeen_cleaned .÷ 20 .* 20
+timerange = 1440:20:2000
+iucn_assesment.decade_last_seen = iucn_assesment.yearLastSeen_cleaned .÷ step(timerange) .* step(timerange)
 
 # Remove html
 cleaned_threats = map(iucn_assesment.threats) do threat
@@ -83,6 +85,7 @@ mean(last.(severity_uncertainty))
 c = count(<(1), last.(severity_uncertainty))
 c / length(severity_uncertainty)
 
+
 # Thread classifications
 function get_flat_threats(threats_dict)
     threatkeys = nothing
@@ -109,27 +112,19 @@ function get_group_dict(group, threats)
     end
     return group_threats
 end
-group_flat_threats = map(groups) do group
-    get_flat_threats(get_group_dict(group, iucn_threats_dict))
-end
-group_flat_threats.amphibian
-allcodes = union(flat_threats.name .=> parse.(Int, first.(split.(flat_threats.code, '.'))))
-group_codes = map(group_flat_threats) do flat_threats
-    union(flat_threats.name .=> parse.(Int, first.(split.(flat_threats.code, '.'))))
-end
 
 cause_labels = [
-    "Residential & commercial development"
+    "Residential & commercial"
     "Agriculture & aquaculture"
     "Energy production & mining"
-    "Transportation & service corridors"
+    "Transport corridors"
     "Biological resource use"
-    "Human intrusions & disturbance"
+    "Human  disturbance"
     "Natural system modifications"
-    "Invasive & other problematic species, genes & diseases"
+    "Invasive & diseases"
     "Pollution"
     "Geological events"
-    "Climate change & severe weather"
+    "Climate & weather"
     "Other options"
 ]
 function count_causes(codes)
@@ -138,22 +133,50 @@ function count_causes(codes)
     end
     DimArray(cs, Dim{:cause}(DimensionalData.Categorical(cause_labels)))
 end
-all_causes = count_causes(allcodes)
+
+flat_threats_with_assesment = leftjoin(flat_threats, iucn_assesment; on=:name => :scientificName)
+
+group_flat_threats = map(groups) do group
+    get_flat_threats(get_group_dict(group, iucn_threats_dict))
+end
+group_threats_with_iucn = map(group_flat_threats) do flat_threats
+    leftjoin(flat_threats, iucn_assesment; on=:name => :scientificName)
+end
+threats_centuries = DimArray(Ti(1400:2000; sampling=Intervals(Start()))) do century
+    map(group_threats_with_iucn) do group
+        filter(group) do row
+            ismissing(row.yearLastSeen_cleaned) ? false : (row.yearLastSeen_cleaned in century..century+99)
+        end
+    end
+end
+# allcodes = union(flat_threats_with_assesment.name .=> parse.(Int, first.(split.(flat_threats.code, '.'))))
+group_codes = map(group_flat_threats) do flat_threats
+    union(flat_threats.name .=> parse.(Int, first.(split.(flat_threats.code, '.'))))
+end
 group_causes = map(count_causes, group_codes)
 cause_matrix = rebuild(cat(group_causes...; dims=Dim{:group}(collect(keys(group_causes)))); name=:count)
-# ctg = repeat(DimensionalData.lookup(cause_matrix, :group), size(cause_matrix, :cause))
-# nam = repeat(DimensionalData.lookup(cause_matrix, :cause), size(cause_matrix, :group))
-# cause_matrix[cause=8:8]
-# groupedbar(nam, cause_matrix, group = ctg, xlabel = "Groups", ylabel = "Scores",
-#     title="", bar_width=0.67, lw=0, framestyle=:box,
-#     xticks= DimensionalData.lookup(cause_matrix, :cause)
-# )
-
+century_group_codes = map(threats_centuries) do century_groups
+    map(century_groups) do flat_threats
+        union(flat_threats.name .=> parse.(Int, first.(split.(flat_threats.code, '.'))))
+    end
+end
+century_group_causes = map(century_group_codes) do group_codes
+    map(count_causes, group_codes)
+end
+century_cause_matrix = map(century_group_causes) do group_causes
+    rebuild(cat(group_causes...; dims=Dim{:group}(collect(keys(group_causes)))); name=:count)
+end
+# all_causes = count_causes(allcodes)
+C = 1700
+for C in 1400:100:2000 
+cause_matrix = century_cause_matrix[Ti=At(C)]
 # Bar plot of all causes by group
 ps = map(1:12) do i
     bar(cause_matrix[cause=i]; ylims=(0, 120))
 end
-Plots.plot(ps...; size=(1900, 1900))
+display(Plots.plot(ps...; size=(1900, 1900)))
+sleep(1)
+end
 
 grouped_codes = Dict{String,Any}()
 for (k, v) in allcodes
@@ -169,22 +192,41 @@ end
 iucn_assesment.threat_codes = species_threat_codes
 bru = filter(:threat_codes => c -> ismissing(c) ? false : 5 in c, iucn_assesment) 
 sort!(iucn_assesment, :yearLastSeen_cleaned)
-threat_timeline = map(x -> Int[], Ti(1440:20:2000))
-for (d, tc) in zip(iucn_assesment.decade_last_seen, iucn_assesment.threat_codes)
-    (ismissing(tc) || ismissing(d)) && continue 
-    a = threat_timeline[At(d)]
-    @show a tc
-    append!(a, tc)
-end
-threat_timeline
-threat_timeline_counts = map(threat_timeline) do xs
-    a = map(Dim{:cause}(DimensionalData.Categorical(1:12))) do i
-        count(==(i), xs)
+
+function get_threat_timeline(df)
+    threat_timeline = map(x -> Int[], Ti(timerange))
+    for (d, tc) in zip(df.decade_last_seen, df.threat_codes)
+        (ismissing(tc) || ismissing(d)) && continue 
+        a = threat_timeline[At(d)]
+        append!(a, tc)
     end
-    set(a, :cause => cause_labels)
+    threat_timeline_counts = map(threat_timeline) do xs
+        a = map(Dim{:cause}(DimensionalData.Categorical(1:12))) do i
+            count(==(i), xs)
+        end
+        set(a, :cause => cause_labels)
+    end
 end
-threat_matrix = cat(threat_timeline_counts...; dims=dims(threat_timeline_counts, Ti))
-Plots.plot(threat_matrix)
+
+as_matrix(timeline) = cat(timeline...; dims=dims(timeline, Ti))
+function get_threat_matrix(df)
+    timeline = get_threat_timeline(df)
+    threat_matrix = as_matrix(timeline)
+    return threat_matrix
+end
+tps = map(collect(pairs(groups))) do (k, group)
+    Plots.plot(get_threat_matrix(group); title="Causes: $k")
+end;
+Plots.plot(tps...; size=(1400, 1800), layout=(4, 2))
+group_threat_timelines = map(get_threat_timeline, groups)
+group_extinction_counts = map(by_decade, groups)
+cause_freqs = map(group_threat_timelines, group_extinction_counts) do tt, ec
+    as_matrix(tt ./ ec)
+end
+rtps = map(collect(pairs(cause_freqs))) do (k, m)
+    Plots.plot(m; title="Cause frequency: $k", legend=:topleft, markerstrokewidth=0)
+end;
+Plots.plot(rtps...; size=(1400, 1800), layout=(4, 2))
 
 flat_threats_path = "/home/raf/PhD/IUCN/flat_threats.csv"
 # CSV.write(flat_threats_path, flat_threats);
@@ -205,26 +247,16 @@ groups = map(class_filters) do filter
 end
 group_sizes = map(nrow, groups) |> pairs
 
-by_decade(df) = by_decade(nrow, df)
+by_decade(df) = by_decade(df) do d
+    n = nrow(d)
+    n == 0 ? missing : n
+end
 function by_decade(f, df)
-    map(Ti(1500:20:2000)) do decade
+    DimArray(Ti(timerange)) do decade
         sub = subset(df, :decade_last_seen => ByRow(==(decade)); skipmissing=true)
         f(sub)
     end
 end
-
-plotgroups = (:bird, :mammal, :reptile, :molusc, :fish, :plant)
-all_extinction_decades = by_decade(iucn_assesment)
-p1 = Plots.scatter(all_extinction_decades; palette=:Dark2_5, markersize=3, label="all", color=:black)
-group_extinction_decades = map(by_decade, groups[plotgroups])
-for (k, dc) in pairs(group_extinction_decades)
-    Plots.scatter!(p1, dc; 
-        title="Extinctions", 
-        label=string(k), markersize=3, opacity=0.5
-    )
-end
-p1
-
 function uncertainty_decade(df)
     by_decade(df) do dc
         if nrow(dc) == 0
@@ -234,23 +266,24 @@ function uncertainty_decade(df)
         end
     end
 end
+
+plotgroups = (:bird, :mammal, :reptile, :molusc, :fish, :plant)
+all_extinction_decades = by_decade(iucn_assesment)
+p1_kw = (palette=:Dark2_5, legend=:topleft, opacity=0.5, markersize=4, markerstrokewidth=0)
+p1 = Plots.scatter(all_extinction_decades; label="all", color=:black, p1_kw...)
+group_extinction_decades = map(by_decade, groups[plotgroups])
+for (k, dc) in pairs(group_extinction_decades)
+    Plots.scatter!(p1, dc; title="Recorded Extinctions", label=string(k), p1_kw...)
+end
 uncertainty_decades = map(uncertainty_decade, groups)
 all_uncertainty_decades = uncertainty_decade(iucn_assesment)
-p2 = Plots.plot(all_uncertainty_decades; 
-    palette=:Dark2_5, markersize=3, label="all", color=:black
-)
+p2_kw = (legend=:bottomcenter, lw=2)
+p2 = Plots.plot(all_uncertainty_decades; label="all", color=:black, p2_kw...)
 group_uncertainty_decades = map(uncertainty_decade, groups[plotgroups])
 for (k, dc) in pairs(group_uncertainty_decades)
-    Plots.plot!(p2, dc; 
-        title="Extinction Cause Uncertainty", 
-        label=string(k), markersize=3, opacity=0.5, legend=:bottomleft
-    )
+    Plots.plot!(p2, dc; label=string(k), title="Extinction Cause Uncertainty", p2_kw...)
 end
-p2
-
 Plots.plot(p1, p2; layout=(2, 1), size=(700, 1200))
-
-p = Plots.scatter(first.(all_extinction_decades), last.(all_extinction_decades); opacity=0.5)
 
 mean_severity_uncertainty(f, x) = mean(f(x.severity_uncertainty))
 mean_citations(f, x) = mean(f(x.citation .== Ref("Yes")))
@@ -267,14 +300,17 @@ function _bar(f, table; mod=identity, plot_kw=(;), p=Plots.bar)
 end
 _bar(mean_text_uncertainty, groups; 
      mod=xs->replace(xs, missing => 1.0),
-     plot_kw=(; color=:lightblue, label="All", title="Extinction Cause Uncertainty"),
+     plot_kw=(; color=:lightblue, label="All", 
+        title="Extinction Cause Uncertainty", 
+        xguide="Cause uncertainty"
+     ),
 )
 _bar(mean_text_uncertainty, groups; 
-     mod=skipmissing, plot_kw=(; color=:blue, label="Assessed"), p=bar!,
+     mod=skipmissing, plot_kw=(; color=:blue, label="Threats\nassessed"), p=bar!,
 )
 
 schema_map = (
-    hunting = ["8.1"],
+    hunting = ["8.1.*"],
     habitat = [""],
     invasives = [""],
     others = [""],
